d81078c3f418ff9833076198ccd11a11f915a9e4
Filip Lönnqvist <fl223km@student.lnu.se>
2025-09-12 11:46:00 +0200

    Started working on facts.js

---
+++ src/data/facts.js
@@ -0,0 +1,26 @@
+/** 
+ * @file Data file containing interesting facts.
+ * @module data/facts
+ * @author Filip Lönnqvist <fl223km@student.lnu.se>
+ * @version 1.0.0
+ */
+
+export const historicalFacts = [
+
+{
+    id: 1,
+    fact: "Did you know that Egyptian queen Cleopatra lived closer in time to the invention of the iPhone than to the building of the Great Pyramid of Giza?",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/3/3e/Kleopatra-VII.-Altes-Museum-Berlin1.jpg",
+    tags: ["ancient", "egypt"],
+    period: "25"
+},
+
+{
+    id: 2,
+    fact: "Did you know that the Roman Emperor Caligula loved his horse Incitatus so much that he gave it a marble stable, servants, and even planned to make it a senator?",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Portrait_Head_of_Caligula_-_Getty_Museum_%2872.AA.155%29.jpg",
+    tags: ["ancient", "rome"],
+    period: "37-41 AD"
+}
+
+]
