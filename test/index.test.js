1b55f2d1b470696152f5279ce959dceeabf9fa89
Filip Lönnqvist <fl223km@student.lnu.se>
2025-09-23 14:57:29 +0200

    getFactsCount, getFactById-group and getFactsByTag tests passed.

--- test/index.test.js
+++ test/index.test.js
@@ -35,10 +35,43 @@ test('getAllFacts should return all facts', () => {
     expect(facts.length).toBe(count)
 })
 
-test('getAllFamilyFriendlyFacts should return all non-explicit facts', () => {
+test('getAllFamilyFriendlyFactsOnly should return all non-explicit facts', () => {
     const facts = historicalFacts.getAllFamilyFriendlyFactsOnly()
 
     for (const fact of facts) {
         expect(fact.isExplicit).toBe(false)
+    }
+})
+
+test('getFactsCount should return the number of facts', () => {
+    const count = historicalFacts.getFactsCount()
+    const facts = historicalFacts.getAllFacts()
+
+    expect(count).toBe(facts.length)
+
+})
+
+describe('getFactById', () => {
+
+    test('should throw an error when ID is not a number', () => {
+        function notANumber() {
+            historicalFacts.getFactById('string')
+        }
+        expect(notANumber).toThrow('ID must be a positive number')
+    })
+
+    test('should throw an error when ID is not a number', () => {
+        function notAPositiveNumber() {
+            historicalFacts.getFactById(-1)
+        }
+        expect(notAPositiveNumber).toThrow('ID must be a positive number')
+    })
+})
+
+test('getFactsByTag should return only facts with specified tag', () => {
+    const facts = historicalFacts.getFactsByTag('war')
+
+    for (const fact of facts) {
+        expect(fact.tags.includes('war')).toBe(true)
     }
 })
