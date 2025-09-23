6fb63e34d895f3b2217e3d6fd6fa11c6932fe02b
Filip Lönnqvist <fl223km@student.lnu.se>
2025-09-23 12:53:47 +0200

    First Jest test coded and passed.

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,10 @@
 import { RandomHistoricalFacts } from '../src/index.js'
 
 const historicalFacts = new RandomHistoricalFacts()
-console.log(historicalFacts.getFact())
+
+
+test('Constructor should create an instance of RandomHistoricalFacts', () => {
+    const testInstance = new RandomHistoricalFacts()
+    const isCorrectType = testInstance instanceof RandomHistoricalFacts
+    expect(isCorrectType).toBe(true)
+})
