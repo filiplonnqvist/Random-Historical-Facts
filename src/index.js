0dae93a52d61cfa713d9d28b4782f40e5bb82b5e
Filip Lönnqvist <fl223km@student.lnu.se>
2025-09-24 11:31:54 +0200

    Final commit before merging to main branch.

--- src/index.js
+++ src/index.js
@@ -163,7 +163,7 @@ export class RandomHistoricalFacts {
   getFactById(id) {
     this.#validateId(id)
 
-    for (const fact of this.facts) { // Loop through each fact
+    for (const fact of this.facts) {
       if (fact.id === id) { // If the ID matches, return the fact
         return { ...fact }
       }
@@ -179,8 +179,8 @@ export class RandomHistoricalFacts {
   getFactsByTag(desiredTag) {
     let result = []
 
-    for (const fact of this.facts) { // Loop through each fact
-      if (!fact.tags) continue // Skip if there are no tags
+    for (const fact of this.facts) {
+      if (!fact.tags) continue
       result = this.#getMatchingTags(result, fact, desiredTag) // Check if any tag matches the desired tag
     }
     if (result.length === 0) {
@@ -198,7 +198,7 @@ export class RandomHistoricalFacts {
    * @returns {Array<Object>} The updated array of matching facts.
    */
   #getMatchingTags(result, fact, desiredTag) {
-    for (const factTag of fact.tags) { // Loop through each tag of the fact
+    for (const factTag of fact.tags) {
       if (this.#isMatchingOutput(factTag, desiredTag)) { // Check if the tag matches the desired tag
         result.push({ ...fact })
         break
@@ -254,7 +254,7 @@ export class RandomHistoricalFacts {
     let result = []
 
     for (const fact of this.facts) { // Loop through each fact
-      if (this.#isMatchingOutput(fact.period, desiredPeriod)) { // Check if the period matches the desired period
+      if (this.#isMatchingOutput(fact.period, desiredPeriod)) { // Check if the existing period matches the desired period input
         result.push({ ...fact })
       }
     }
@@ -332,10 +332,10 @@ export class RandomHistoricalFacts {
     this.#validateFactsAvailability()
 
     const result = [...this.facts]
-    if (ascending) { // Sort in ascending order
+    if (ascending) {
       result.sort((a, b) => a.year - b.year)
     } else {
-      result.sort((a, b) => b.year - a.year) // Sort in descending order
+      result.sort((a, b) => b.year - a.year)
     }
     return result
   }
